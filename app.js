e7786e57a614844291bc117b1513c3f40dcfc98d
BarenneM <barenne.marie@outlook.com>
2021-02-16 13:04:06 +0100

    rapports

--- app.js
+++ app.js
@@ -24,7 +24,50 @@ const visiteurs = document.querySelector('#visiteurs');
 // });
 
 //Récupérer un rapport par son ID  --> GET http://localhost:90/gsb/rapport/{id}
-//Poster un rapport --> POST http://localhost:90/gsb/rapport
+
+//Poster un rapport --> POST  http://localhost:90/gsb/visiteur/1/rapport
+const keyword = document.querySelector('#keyword');
+const bouton = document.querySelector("#bouton");
+
+submitRapports.addEventListener('click', (event) => {
+    event.preventDefault();
+    const nomMed = document.querySelector('#nomMed');
+    const bilan = document.querySelector('#bilan');
+    const radioButtonConviced = document.querySelectorAll('[name= "isConvinced"]');
+    const radioButtonReexaminated = document.querySelectorAll('[name= "isReexamined"]');
+    const medicaments = document.querySelector('#medicaments');
+    
+    const nomMedValue = nomMed.value;
+    const bilanValue = bilan.value;
+    const radioButtonConvicedValue = radioButtonConviced.value;
+    const radioButtonReexaminatedValue = radioButtonReexaminated.value;
+    const medicamentsValue = medicaments.value;
+
+
+    console.log(nomMedValue);
+    console.log(bilanValue);
+    console.log(radioButtonConvicedValue);
+    console.log(radioButtonReexaminatedValue);
+    console.log(medicamentsValue);
+
+    // const url = 'http://localhost:90/gsb/visiteur/1/rapport';
+
+    // fetch(url, {
+    //     method: 'POST',
+    //     body: JSON.stringify({query: event.currentTarget.value })
+    // })
+    // .then(response => response.json())
+    // .then((data) => {
+    //     console.log(data);
+    // })
+    // .catch((e) => {
+    //     console.log(e);
+    // });
+});
+
+
+
+
 //Modifier un rapport --> PUT http://localhost:90/gsb/rapport/{id}
 //Supprimer un rapport --> DELETE http://localhost:90/gsb/rapport/{id}
 
@@ -43,7 +86,7 @@ fetch(url)
                 visiteurs.insertAdjacentHTML('beforeend', `
                 <li class="usersList"> 
                     <h4> ${visiteur.nom} </h4>
-                    <p Matricule : ${visiteur.id} </p>
+                    <p> Matricule : ${visiteur.id} </p>
                 </li>
                 `);
             });
